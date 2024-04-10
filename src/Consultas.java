3aa7723ecb7fe99b57ac4a16e7458a120057e987
angelsl003 <113622992+angelsl003@users.noreply.github.com>
2024-04-10 20:12:23 +0200

    Add files via upload

---
+++ src/Consultas.java
@@ -0,0 +1,41 @@
+import Ejemplos.Singleton.DatabaseConnection;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Consultas {
+    static java.sql.Connection con= DatabaseConnection.getInstance().getConnection();
+    public Consultas(){
+
+    }
+
+    public static void codigosPaises(int pais) throws SQLException {
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try{
+            statement = con.createStatement();
+            resultSet = statement.executeQuery("select nombre_pais from paises p where p.cod_pais="+pais);
+            while (resultSet.next()){
+                System.out.println(resultSet.getString(1));
+            }
+        }catch (SQLException e){
+            System.out.println("Error "+e.getMessage());
+        }finally {
+            try{
+                if(statement!=null){
+                    statement.close();
+                }
+            }catch (java.sql.SQLException ex){
+                System.out.println("Error "+ex.getMessage());
+            }
+            try {
+                if(resultSet!=null){
+                    resultSet.close();
+                }
+            }catch (java.sql.SQLException ex){
+                System.out.println("Error "+ex.getMessage());
+            }
+        }
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        //usuario:angel123
+        //pass:1234
+
+    }
+}
