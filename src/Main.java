35dcf65659becd5ad1865b29a3b080e8d7bdc62f
DMM <minodora@uoc.edu>
2024-04-11 16:13:31 +0200

    menú empezado

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,31 @@
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
         //usuario:angel123
         //pass:1234
+
+        int opcionUsuario = 0;
+        Scanner miEscaner = new Scanner(System.in);
+
+        while(opcionUsuario != -1){
+            System.out.println("1) Consultar código de país");
+            System.out.println("2) Consultar jugadores por código de país");
+            System.out.println("\n Elige una opción. Introduce -1 para salir");
+            opcionUsuario = miEscaner.nextInt();
+
+            switch(opcionUsuario){
+                case -1:
+                    break;
+                case 1:
+                    System.out.println("¿Cuál es el código de país que quieres consultar?");
+                    int codigoPaisUsr = miEscaner.nextInt();
+                    Consultas.codigosPaises(codigoPaisUsr);
+                default:
+                    System.out.println("Opción no válida");
+            }
+        }
 
     }
 }
